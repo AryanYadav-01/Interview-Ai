47f295642202ad51ea48e6a5bb116a6d62dd2b2c
Aryan Yadav <ay630147gmail.com@>
2026-05-06 00:58:50 +0530

    fix chrome executablePath

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -61,12 +61,18 @@ async function generatePdfFromHtml(htmlContent) {
     const browser = await puppeteer.launch({
         headless: "new",
         executablePath: "/opt/render/.cache/puppeteer/chrome/linux-147.0.7727.57/chrome-linux64/chrome",
-        args: ["--no-sandbox", "--disable-setuid-sandbox"]
+        args: [
+            "--no-sandbox",
+            "--disable-setuid-sandbox",
+            "--disable-dev-shm-usage",
+            "--disable-gpu"
+        ]
     });
     const page = await browser.newPage();
     await page.setContent(htmlContent, { waitUntil: "networkidle0" })
     const pdfBuffer = await page.pdf({
-        format: "A4", margin: {
+        format: "A4",
+        margin: {
             top: "20mm",
             bottom: "20mm",
             left: "15mm",
